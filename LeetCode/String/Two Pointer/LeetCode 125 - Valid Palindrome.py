8ac554d447d3bc7f521555bcca89b4d43f0e14cb
Aviral Janveja <aviraljanveja@gmail.com>
2025-06-27 23:51:48 +0530

    LeetCode 125 - Valid Palindrome

--- LeetCode/String/Two Pointer/LeetCode 125 - Valid Palindrome.py
+++ LeetCode/String/Two Pointer/LeetCode 125 - Valid Palindrome.py
@@ -2,22 +2,22 @@
 # Problem Link : https://leetcode.com/problems/valid-palindrome/
 
 def isPalindrome(s):
-    # First section : Cleaning the input string.
-    val = "0123456789abcdefghijklmnopqrstuvwxyz"  # Define a string containing all valid alphanumeric characters
-    s2 = ""  # Initialize an empty string to store the cleaned version of the input
-    for char in s.lower():  # Convert the input string to lowercase and iterate through each character
-        if char in val:  # If the character is alphanumeric, add it to the cleaned string
-            s2 += char
+    s1 = ""  # Initialize an empty string to store the cleaned version of the input
+    for char in s:  # Iterate through each character
+        if char.isalnum():  # If the character is alphanumeric, add it to the cleaned string
+            s1 += char.lower()
 
-    # Second section : Two-pointer technique used to check for a palindrome.
-    i = 0  # Pointer 1 : Start of string
-    j = len(s2) - 1  # Pointer 2 : End of String
-    while i < j:  # Use a while loop to compare characters from both ends moving towards the center
-        if s2[i] != s2[j]:  # If the characters at the current pointers do not match, return False (not a palindrome)
-            return False
-        i += 1  # Move the left pointer to the right
-        j -= 1  # and the right pointer to the left
-    return True  # If the loop completes without finding a mismatch, return True (it is a palindrome)
+    return s1 == s1[::-1]  # Check if the cleaned string is equal to its reverse (palindrome check)
+
+    # Second option : Two-pointer technique to check for a palindrome.
+    # i = 0  # Pointer 1 : Start of string
+    # j = len(s1) - 1  # Pointer 2 : End of String
+    # while i < j:  # Use a while loop to compare characters from both ends moving towards the center
+    #     if s1[i] != s1[j]:  # If the characters at the current pointers do not match, return False (not a palindrome)
+    #         return False
+    #     i += 1  # Move the left pointer to the right
+    #     j -= 1  # and the right pointer to the left
+    # return True  # If the loop completes without finding a mismatch, return True (it is a palindrome)
 
 
 # Test cases
