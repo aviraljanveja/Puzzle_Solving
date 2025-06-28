30ad79c921c4d3cc13ddbf4c0116833b23221996
Aviral Janveja <aviraljanveja@gmail.com>
2025-06-28 17:32:58 +0530

    LeetCode 121

--- LeetCode/Array/Two Pointers/LeetCode 121 - Best Time to But and Sell Stock.py
+++ LeetCode/Array/Two Pointers/LeetCode 121 - Best Time to But and Sell Stock.py
@@ -1,21 +1,19 @@
-# LeetCode 121 - Best Time to Buy and Sell Stock
-# (https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/)
+# LeetCode 121 : Best Time to Buy and Sell Stock
+# Problem Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 
-def maxProfit(price):
-    buy = 0  # Pointer 1 = Buy
-    sell = 0  # Pointer 2 = Sell
+def maxProfit(prices):
+    buy = 0  # Pointer 1
+    sell = 1  # Pointer 2
     max_profit = 0  # Variable to store the maximum profit encountered
 
-    while sell < len(price):  # Iterate till sell value reaches the end of price list
-        if price[buy] < price[sell]:  # Check if the current "buy" price is less than the current "sell" price
-            profit = price[sell] - price[buy]  # If yes, calculate profit
-            if profit > max_profit:  # Update max_profit if the current profit is greater
+    while sell < len(prices):  # Iterate till sell pointer reaches the end of list
+        if prices[buy] < prices[sell]:  # Check if the current buy-price is less than the current sell-price
+            profit = prices[sell] - prices[buy]  # If yes, calculate profit
+            if profit > max_profit:  # Update max_profit, if the current profit is greater
                 max_profit = profit
-
-        else:  # If current buy price is more than sell, update buy pointer to current sell pointer
-            buy = sell
-
-        sell += 1
+        else:  # Instead, if current buy-price is more than sell-price, update buy pointer,
+            buy = sell # so that we can buy at the lowest available price.
+        sell += 1  # Increment sell pointer to check for the next profit margin
 
     return max_profit  # Return the maximum profit found
 
