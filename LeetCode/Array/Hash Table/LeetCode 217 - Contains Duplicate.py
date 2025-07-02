fbb2a9b7f8d545f83201ba650869ade3ce4f11e9
Aviral Janveja <aviraljanveja@gmail.com>
2025-07-02 23:56:29 +0530

    LeetCode 217

--- LeetCode/Array/Hash Table/LeetCode 217 - Contains Duplicate.py
+++ LeetCode/Array/Hash Table/LeetCode 217 - Contains Duplicate.py
@@ -1,14 +1,14 @@
-# LeetCode 217 - Contains Duplicate (https://leetcode.com/problems/contains-duplicate/description/)
+# LeetCode 217 : Contains Duplicate
+# Problem Link : https://leetcode.com/problems/contains-duplicate/description/
 
 def containsDuplicate(nums):
-    # Initialize an empty set to keep track of numbers we have seen so far.
-    # A set is used because it provides fast look-up : O(1) average time complexity.
-    check = set()
+    check = set()  # Initialize an empty set to keep track of numbers we have seen so far.
 
-    for n in nums:  # Iterate through the numbers in the input list.
-        if n in check:  # Check if the current number is already in the set.
+    for n in nums:  # Iterate through the array.
+        if n in check:  # Check if the current number is already in the array.
             return True  # If it is, we have found a duplicate, so return True.
-        check.add(n)  # Otherwise, add the current number to the set.
+        else:
+            check.add(n)  # else, add the current number to the set.
 
     return False  # If the loop exits without finding any duplicates, return False.
 
