aaabc03b87422a4c618f6027e1b01d527064ca67
Aviral Janveja <aviraljanveja@gmail.com>
2025-07-06 01:12:51 +0530

    add fundamental data structure - stack

---
+++ Fundamentals/6 - Data Structures/Stack/Stack_Intro.py
@@ -0,0 +1,35 @@
+# A stack is a data structure where the last element added is the first one to be removed.
+# This way of organizing elements is called LIFO : Last In First Out.
+
+# Basic operations we can do on a stack are:
+# - Push: Adds a new element on the stack.
+# - Pop: Removes and returns the top element from the stack.
+# - Peek: Returns the top (last) element on the stack.
+# - isEmpty: Checks if the stack is empty.
+# - Size: Finds the number of elements in the stack.
+
+# Stacks can be used to implement undo mechanisms, to revert to previous states,
+# to create algorithms for depth-first search in graphs, or for backtracking.
+
+stack = []
+
+# Push
+stack.append('A')
+stack.append('B')
+stack.append('C')
+print(stack)  # Output = ['A', 'B', 'C']
+
+# Peek
+print(stack[-1])  # Output = C
+
+# Pop
+print(stack.pop())  # Output = C
+
+# Stack after Pop
+print(stack)  # Output = ['A', 'B']
+
+# IsEmpty
+print(bool(stack))  # Output = True
+
+# Size
+print(len(stack))  # Output = 2
