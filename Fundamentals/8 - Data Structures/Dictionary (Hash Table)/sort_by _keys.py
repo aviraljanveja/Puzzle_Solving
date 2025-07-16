85d36141156a666232b492a99aa04476fc962983
Aviral Janveja <aviraljanveja@gmail.com>
2025-07-16 20:49:19 +0530

    fundamentals folder update II

--- Fundamentals/8 - Data Structures/Dictionary (Hash Table)/sort_by _keys.py
+++ Fundamentals/8 - Data Structures/Dictionary (Hash Table)/sort_by _keys.py
@@ -4,7 +4,7 @@ squares = {3: 9, 1: 1, 2: 4}
 print(squares)  # Output = {3: 9, 1: 1, 2: 4}
 
 # To sort a dictionary by its keys, you can use the sorted() function
-# which returns the sorted key,value pairs as tuples (key, value)
+# which returns a list of sorted key-value pairs as tuples.
 # They can then be converted back to dictionary using the dict() constructor
 sorted_squares = dict(sorted(squares.items()))
 print(sorted_squares)  # Output = {1: 1, 2: 4, 3: 9}
