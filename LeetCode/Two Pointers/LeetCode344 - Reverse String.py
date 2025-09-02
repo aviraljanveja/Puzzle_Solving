75699afcbfb0446289b255b1a8bf2fadf13acdbb
Aviral Janveja <aviraljanveja@gmail.com>
2025-09-02 10:27:50 +0530

    add leetcode344 to problem solving

---
+++ LeetCode/Two Pointers/LeetCode344 - Reverse String.py
@@ -0,0 +1,20 @@
+# LeetCode 344 : Reverse String
+# Problem Link : https://leetcode.com/problems/reverse-string/description/
+
+def reverseString(s):
+    l = 0  # pointer 1 : l at the start of the string
+    r = len(s)-1  # pointer 2 : r at the end of the string
+    while l < r:  # Iterate only while l is less than r, as we need not move the middle character in an odd length string
+        s[l], s[r] = s[r], s[l]  # Keep swapping the characters from the start and the end
+        l += 1  # Increment the left pointer
+        r -= 1  # Decrement the right pointer
+
+
+# Test cases:
+s1 = ["h","e","l","l","o"]
+reverseString(s1)
+print(s1)
+
+s2 = ["H","a","n","n","a","h"]
+reverseString(s2)
+print(s2)
