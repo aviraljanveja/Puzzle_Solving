9f07547a9fb4890afd53bb0dc5ebfd62254449f1
Aviral Janveja <aviraljanveja@gmail.com>
2025-09-02 09:41:06 +0530

    add leetcode20 to problem solving

--- LeetCode/Stack/LeetCode20 - Valid Parentheses.py
+++ LeetCode/Stack/LeetCode20 - Valid Parentheses.py
@@ -2,19 +2,19 @@
 # Problem Link : https://leetcode.com/problems/valid-parentheses/
 
 def isValid(s):
-    pairs = {')':'(', '}':'{', ']':'['}  # Dictionary to store valid pairs of opening and closing brackets.
+    brackets = {')':'(', '}':'{', ']':'['}  # Dictionary to store pairs of opening and closing brackets.
     stack = []  # Initiating an empty Stack, implemented via the Python list.
 
     for char in s:  # Iterate over the characters in the given string.
-        if char in pairs:  # if the character is a closing bracket (one of the keys of the "pairs" dictionary defined above)
-            if stack and stack[-1] == pairs[char]:  # Then check if the stack is non-empty and if the last character added to the stack was the corresponding opening bracket
-                stack.pop()  # If yes, then it is a valid closing bracket and we pop it off the stack, essentially taking care of one valid pair of brackets.
+        if char in brackets:  # if the current character is a closing bracket (one of the keys in the dictionary defined above)
+            if stack and stack[-1] == brackets[char]:  # Then check if the stack is non-empty and if the last character added to the stack was the corresponding opening bracket
+                stack.pop()  # If yes, then it is a valid pair, so we pop it off the stack.
             else:
-                return False  # If not, then either the order or type of brackets is incorrect and hence we return False.
+                return False  # If not, then either the order or type of brackets is incorrect and hence function returns False.
         else:
-            stack.append(char)  # On the other hand, if the current character is an opening bracket, then simply add it to the stack.
+            stack.append(char)  # On the other hand, if the current character is an opening bracket, then simply add it on top of the stack.
 
-    return len(stack) == 0  # If the stack is empty in the end, all the bracket-pairs were valid and popped off the stack, thus we return True, otherwise False.
+    return len(stack) == 0  # If the stack is empty in the end, it means all the bracket-pairs were valid and popped off the stack, thus we return True, otherwise False.
 
 
 # Test cases
