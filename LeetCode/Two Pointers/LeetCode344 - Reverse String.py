74d86ec71d9b6768e9884f8b74aa1d478686f716
Aviral Janveja <aviraljanveja@gmail.com>
2025-09-09 10:51:21 +0530

    update LeetCode344

--- LeetCode/Two Pointers/LeetCode344 - Reverse String.py
+++ LeetCode/Two Pointers/LeetCode344 - Reverse String.py
@@ -2,8 +2,8 @@
 # Problem Link : https://leetcode.com/problems/reverse-string/description/
 
 def reverseString(s):
-    l = 0  # pointer 1 : l at the start of the string
-    r = len(s)-1  # pointer 2 : r at the end of the string
+    l = 0  # Left pointer at the start of the string
+    r = len(s)-1  # Right pointer at the end of the string
     while l < r:  # Iterate only while l is less than r, as we need not move the middle character in an odd length string
         s[l], s[r] = s[r], s[l]  # Keep swapping the characters from the start and the end
         l += 1  # Increment the left pointer
