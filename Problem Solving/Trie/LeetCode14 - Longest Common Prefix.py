991ffd2069afcf287211bd2ea97c5cf757877649
Aviral Janveja <aviraljanveja@gmail.com>
2025-09-10 14:14:38 +0530

    Add LeetCode14

---
+++ Problem Solving/Trie/LeetCode14 - Longest Common Prefix.py
@@ -0,0 +1,18 @@
+# LeetCode 14 : Longest Common Prefix
+# Problem Link : https://leetcode.com/problems/longest-common-prefix/description/
+
+def longestCommonPrefix(strs):
+    for i in range(len(strs[0])):  # We pick the first string from the list of strings and check if its characters,
+        for s in strs:  # match across the rest of the strings in the list.
+            if i == len(s) or strs[0][i] != s[i]:  # As soon as some string length is reached, or we find a character mismatch,
+                return strs[0][:i]  # we return the slice of string upto the matching character length as our common prefix
+
+    return strs[0]  # If all characters of the first string are common across all strings, then it naturally becomes our longest common prefix
+
+
+# Test Cases :
+str1 = ["flower", "flow", "flight"]
+print(longestCommonPrefix(str1))  # Output = "fl"
+
+str2 = ["dog", "racecar", "car"]
+print(longestCommonPrefix(str2))  # Output = ""
