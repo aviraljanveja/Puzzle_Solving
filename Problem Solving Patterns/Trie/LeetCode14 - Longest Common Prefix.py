825d58c9c24f75d6daeb0f1c2f27ad7f4be3fc75
Aviral Janveja <aviraljanveja@gmail.com>
2025-09-15 17:05:47 +0530

    Leetcode14 minor update

--- Problem Solving Patterns/Trie/LeetCode14 - Longest Common Prefix.py
+++ Problem Solving Patterns/Trie/LeetCode14 - Longest Common Prefix.py
@@ -5,7 +5,7 @@ def longestCommonPrefix(strs):
     for i in range(len(strs[0])):  # We pick the first string from the list of strings and check if its characters,
         for s in strs:  # match across the rest of the strings in the list.
             if i == len(s) or strs[0][i] != s[i]:  # As soon as some string length is reached, or we find a character mismatch,
-                return strs[0][:i]  # we return the slice of string upto the matching character length as our common prefix
+                return strs[0][:i]  # we return the slice of the first string upto the matching character length "i" as our common prefix
 
     return strs[0]  # If all characters of the first string are common across all strings, then it naturally becomes our longest common prefix
 
