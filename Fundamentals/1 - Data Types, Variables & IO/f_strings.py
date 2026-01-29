d1358f305cf53bb59208ce0be9e6e2c582e60665
Aviral Janveja <aviraljanveja@gmail.com>
2026-01-29 21:09:47 +0530

    Problem Solving ReStruct 8

--- Fundamentals/1 - Data Types, Variables & IO/f_strings.py
+++ Fundamentals/1 - Data Types, Variables & IO/f_strings.py
@@ -1,10 +1,9 @@
-# f-strings (formatted string literals) in Python
-# provide a way to embed expressions inside string literals using curly braces {}.
+# f-strings (formatted strings) in Python
+# provide a way to embed expressions inside strings using curly braces {}.
 # They were introduced in Python 3.6 to make string formatting more readable, concise and efficient.
 
 # An f-string is a string that is prefixed with an f or F and allows
 # you to insert expressions inside curly braces {} directly within the string.
-# These expressions are evaluated at runtime and formatted using the __format__() method.
 
 # Basic Example
 name = "Avi"
