1655dd5957a6c69531f7c40ea3282d2fbad0ce2d
Aviral Janveja <aviraljanveja@gmail.com>
2026-01-30 20:42:59 +0530

    Problem Solving ReStruct 12

--- 1 - Fundamentals/2 - Operators/Arithmetic Operators.py
+++ 1 - Fundamentals/2 - Operators/Arithmetic Operators.py
@@ -1,9 +1,11 @@
-# Common Arithmetic Operators
+# Common Arithmetic Operators :
 
 a = 5
 b = 3
 
-print(a+b)  # Addition
-print(a-b)  # Subtraction
-print(a*b)  # Multiplication
-print(a/b)  # Division
+print(a + b)  # Addition, Output = 8
+print(a - b)  # Subtraction, Output = 2
+print(a * b)  # Multiplication, Output = 15
+print(a / b)  # Float Division, Output = 1.6666666666666667
+print(a // b)  # Integer Division, Output = 1
+print(a ** b)  # Exponent, 5 raised to power 3, Output = 125
--- 1 - Fundamentals/2 - Operators/Exponent.py
+++
@@ -1,6 +0,0 @@
-# Exponent Operator
-
-a = 5
-b = 2
-
-print(a ** b)  # 5 raised to power 2, Output = 25
