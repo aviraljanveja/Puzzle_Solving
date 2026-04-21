2953a92d19a4f5d805473ded918e5e22ebcdb8cf
Aviral Janveja <aviraljanveja@gmail.com>
2026-04-21 09:49:12 +0530

    fundemantals ch3 file changes

--- Fundamentals/3 - Branching/if_elif_else.py
+++ Fundamentals/3 - Branching/if_elif_else.py
@@ -1,10 +1,21 @@
-# The if-elif-else chain uses elif (short for "else if") for multiple conditions.
+# Basic if-else : The if condition is evaluated, if it is True the code inside the if-block runs,
+# otherwise the code inside the else-block runs.
 
 x = int(input("Enter a number : "))
 
-if x > 0:
+if x % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+# The if-elif-else chain allows you to check multiple conditions sequentially.
+# The first condition that evaluates to True will have its block executed, and the rest will be skipped.
+y = int(input("Enter a number : "))
+
+if y > 0:
     print("Positive")
-elif x < 0:
+elif y < 0:
     print("Negative")
 else:
     print("Zero")
--- Fundamentals/3 - Branching/if_else.py
+++
@@ -1,8 +0,0 @@
-# Basic if-else : The else block runs when the if-condition is False.
-
-x = int(input("Enter a number : "))
-
-if x % 2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")
