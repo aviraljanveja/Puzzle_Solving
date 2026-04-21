cf72416578c14b5dd90613cd80739367ed8d471e
Aviral Janveja <aviraljanveja@gmail.com>
2026-04-21 10:07:36 +0530

    fundemantals ch4 for loop changes

---
+++ Fundamentals/4 - Iteration/for.py
@@ -0,0 +1,54 @@
+# for Loop : A for loop is used to iterate over a sequence ( for example, string) or other iterable objects. 
+# It executes a block of code for each item in the sequence.
+
+
+text = "aviral"
+
+
+# Iterating directly over values
+for char in text:
+    print(char, end = " ")  # Output = a v i r a l
+
+
+print()  # for new line
+
+
+# Iterating over indices
+for i in range(len(text)):
+    print(f"{text[i]}:{i}", end = " ")  # Output = a:0 v:1 i:2 r:3 a:4 l:5
+
+
+print()  # for new line
+
+
+# Reverse iteration over values using slicing. The [::-1] slice creates a reversed copy of the string.
+for char in text[::-1]:
+    print(char, end = " ")  # Output = l a r i v a
+
+
+print()  # for new line
+
+
+# Reverse iteration using the range function with a negative step.
+for i in range(len(text)-1, -1, -1):
+    print(f"{text[i]}:{i}", end = " ")  # Output = l:5 a:4 r:3 i:2 v:1 a:0
+
+
+print()  # for new line
+
+
+# break : Terminates the loop early.
+for i in range(10):
+    if i == 5:
+        break
+    print(i, end = " ")  # Output = 0 1 2 3 4
+
+
+print()  # for new line
+
+
+# continue : Skips the current iteration and moves on to the next.
+for i in range(10):
+    if i == 5:
+        continue
+    print(i, end = " ")  # Output = 0 1 2 3 4 6 7 8 9
